7e4e0cdf9921ee2ee6ba3f8c18d15ddee886f99d
Daniela Hirsch <danielahirsch@Danielas-MacBook-Pro.local>
2025-07-23 09:53:01 +0300

    Initial commit

---
+++ FitNote/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FitNote
+//
+//  Created by Daniela Hirsch on 23/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FitNote/FitNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitNoteApp.swift
+//  FitNote
+//
+//  Created by Daniela Hirsch on 23/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct FitNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
